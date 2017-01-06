82976c117132fb6385127220550a0f36da6d5919
mike <mikesavior@users.noreply.github.com>
2017-01-06 18:50:43 -0500

    Update orangeControl.js

--- orangeControl.js
+++ orangeControl.js
@@ -1,12 +1,12 @@
 function addTwo(a, b, string) {
         return { result: (a + b), food: ('' + string) };
-    }
+}
 
 function newThing (startedWith, result, food) {
-    this.startedWith = startedWith,
+	this.startedWith = startedWith,
 	this.addition = result;
 	this.food = food;
-    }
+}
 var orangeControl = new newThing(0, 169, "oranges");
 addTwo(orangeControl.startedWith, orangeControl.addition, orangeControl.food);
 //enjoy some oranges.
